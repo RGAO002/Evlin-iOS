70938a5ade37f2691bfd955d80230d29b997744b
Ruoping Gao <gruoping@gmail.com>
2026-04-04 17:35:19 -0400

    Initial Commit

---
+++ Evlin iOS/Evlin_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Evlin_iOSApp.swift
+//  Evlin iOS
+//
+//  Created by Ruoping Gao on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Evlin_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
