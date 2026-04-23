2c6010a4736dd0c6d154135a4f529873b7032e34
Ruoping Gao <gruoping@gmail.com>
2026-04-22 20:23:41 -0400

    chore(xcode): add Evlin iOSTests target + scheme updates

---
+++ Evlin iOSTests/Evlin_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  Evlin_iOSTests.swift
+//  Evlin iOSTests
+//
+//  Created by Ruoping Gao on 4/22/26.
+//
+
+import Testing
+
+struct Evlin_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
